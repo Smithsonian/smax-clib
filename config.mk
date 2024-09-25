ccfba7adc48ef10fe36d463bec1604f2770ee6a6
Attila Kovacs <attipaci@gmail.com>
2024-09-25 15:43:18 +0200

    Add search LIB to LDFLAGS also

--- config.mk
+++ config.mk
@@ -86,6 +86,7 @@ LDFLAGS += $(THREADS) -lredisx -lxchange
 
 # Search for libraries under LIB
 ifneq ($(findstring $(LIB),$(LD_LIBRARY_PATH)),$LIB)
+  LDFLAGS += -L$(LIB)
   LD_LIBRARY_PATH := $(LIB):$(LD_LIBRARY_PATH)
 endif
 
