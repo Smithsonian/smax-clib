65a545ede9d452c7af74ace31bd1dc7bb3ca4582
Attila Kovacs <attipaci@gmail.com>
2024-12-10 13:48:21 +0100

    A poorly initialized XField

--- src/smax.c
+++ src/smax.c
@@ -755,7 +755,7 @@ int smaxShare(const char *table, const char *key, const void *value, XType type,
 int smaxShareArray(const char *table, const char *key, const void *ptr, XType type, int ndim, const int *sizes) {
   static const char *fn = "smaxShareArray";
 
-  XField f = {NULL};
+  XField f = X_FIELD_INIT;
   char trybuf[REDISX_CMDBUF_SIZE];
   int count, status;
 
