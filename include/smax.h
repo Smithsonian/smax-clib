4ec180f94f85ec0a550912a7adc71ec3c03bf6a4
Attila Kovacs <attipaci@gmail.com>
2025-07-08 20:52:43 +0200

    v1.0.0-rc1

--- include/smax.h
+++ include/smax.h
@@ -59,7 +59,7 @@
 #define SMAX_PATCHLEVEL     0
 
 /// Additional release information in version, e.g. "-1", or "-rc1".
-#define SMAX_RELEASE_STRING "-devel"
+#define SMAX_RELEASE_STRING "-rc1"
 
 /// \cond PRIVATE
 
